68bff7630d4979989d959d035eeaebb3da0baf59
handarudwiki <handarudwiki04@gmail.com>
2025-04-16 14:15:52 +0700

    init project and create migrations

---
+++ drizzle/0000_medical_patch.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "orders" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"product_id" integer NOT NULL,
+	"quantity" integer NOT NULL,
+	"total_price" numeric(10, 2) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "products" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"description" varchar(255) NOT NULL,
+	"price" numeric(10, 2) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "public"."products"("id") ON DELETE cascade ON UPDATE no action;
---
+++ src/db/schema.ts
@@ -0,0 +1,34 @@
+import { relations } from "drizzle-orm";
+import { decimal, numeric, pgTable, serial, timestamp } from "drizzle-orm/pg-core";
+import { integer } from "drizzle-orm/pg-core";
+import {  varchar } from "drizzle-orm/pg-core";
+
+export const product = pgTable("products", {
+    id: serial().primaryKey(),
+    name: varchar({length: 255}).notNull(),
+    description: varchar({length: 255}).notNull(),
+    price: numeric({precision: 10, scale:2}).notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const order = pgTable("orders", {
+    id: serial().primaryKey(),
+    productId: integer("product_id").notNull().references(() => product.id, {
+        onDelete: "cascade",}),
+    quantity: integer().notNull(),
+    total_price : decimal({precision: 10, scale:2}).notNull(),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const orderRelation = relations(order, ({ one }) => ({
+    product: one(product, {
+        fields: [order.productId],
+        references: [product.id],
+    }),
+}));
+
+export const productRelation = relations(product, ({ many }) => ({
+    orders: many(order),
+}));
